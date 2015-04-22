6afbf019900efd6533dd3ddedf8b6719b93b62c3
Steve Nester <steve@sahfor.com>
2015-04-22 02:58:40 -0700

    backoffice

---
+++ src/main/java/com/payon/servertoserver_java/Backoffice_REVERSAL.java
@@ -0,0 +1,41 @@
+package com.payon.servertoserver_java;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import org.apache.commons.io.IOUtils;
+
+public class Backoffice_REVERSAL {
+	public static void main(String[] args) throws IOException {
+		System.out.println(new Backoffice_REVERSAL().backofficeOperation());
+	}
+	private String backofficeOperation() throws IOException {
+		URL url = new URL("https://test.oppwa.com/v1/payments/8a8294494cdc8e92014ce08dc0b42abc");
+
+		HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+		conn.setRequestMethod("POST");
+		conn.setDoInput(true);
+		conn.setDoOutput(true);
+		
+		String data = "authentication.userId=8a8294174b7ecb28014b9699220015cc"
+				+ "&authentication.password=sy6KJsT8" 
+				+ "&authentication.entityId=8a8294174b7ecb28014b9699a3cf15d1"
+				+ "&paymentType=RV";
+
+		DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+		wr.writeBytes(data);
+		wr.flush();
+		wr.close();
+		int responseCode = conn.getResponseCode();
+		InputStream is;
+	    
+		if (responseCode >= 400) is = conn.getErrorStream();
+		else is = conn.getInputStream();
+	    
+		return IOUtils.toString(is);
+	}
+}
